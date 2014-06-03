386f674796e97d509cba6bc5490d4fe7f4a9bee0
david peach <dpeach27@gmail.com>
2014-06-03 21:38:58 +0100

    Updated readme. Edited quick start guide.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-A WordPress plugin for adding custom Amazon Associates links into your posts.
+# Amazon Associates For Posts
+
+**A WordPress plugin for adding custom Amazon Associates links into your posts.**
+
+Referral image links are images that — when clicked — link to a chosen product on Amazon's website. You will then receive commission when people buy the product as a result of your website's referral link.
+
+This plugin helps you to insert custom Amazon Associates referral image links into your post content.
+
+Once you've enabled it on a post, you will see your referral image link towards the top of your post, floated to the right.
+
+Please Note: You will need an Amazon Associates account to take advantage of this plugin. You can find out more here.
+
+## Getting Started:
+
+1. Head over to the Amazon Associates site. Register / Login and head to your dashboard.
+2. On your dashboard page you have the option of searching/browsing for a product. Find a product that you want to link to and click the yellow dropdown labelled 'Get Link'
+3. Inside that drop down you will have a referral link to the chosen product, which you will need for use in this plugin.
+4. Once you've pasted the link somewhere safe, note the product title and type of product it is (album/film/book/etc). These will all be used in the plugin also.
+5. Finally, you will need an image of the product that will display on your site. Please check the copyright of the image you are using before using it. I presume images taken from Amazon would be okay as it is their affiliate program but I am not 100% sure. Please check.
+6. Now you have two ways of attaching your referral image link to your post.
+..* In the editor for that particular post. (Towards the bottom of the page)
+..* On the 'Amazon Links' page of the plugin. (All of your published posts will be listed here for the quick adding of multiple image links)
+7. Whichever way you choose, simply complete the fields with the relevant information and alter 'Disable' to 'Enable'.
+8. To stop the image from being displayed on your site change the status back to 'Disabled'.
+9. To remove the settings for a particular post, check the box labelled 'Clear From Post' and click the update button.
+
+### Notes:
+
+At the moment when showing an amazon referral link on a post, if that post normally has a featured image on it's single post page, it will not be displayed. To show the featured image again, set the referral link status back to 'Disabled'.
--- amazon-associates-posts.php
+++ amazon-associates-posts.php
@@ -88,18 +88,18 @@ class AmazonAssociatesPosts {
 		?>
 		<div class='wrap'>
 		<h2><?php echo $this->plugin_display_name; ?></h2>
-		<p>Referrer image links are images that&#8202;&mdash;&#8202;when clicked&#8202;&mdash;&#8202;link to a chosen product on Amazon's website. You will then receive commission when people buy the product as a result of your website's link.</p>
-		<p>This plugin helps you to insert custom Amazon Associates referrer image links into your post content.</p>
-		<p>Once you've enabled it on a post, you will see your referrer image link towards the top of your post, floated to the right.</p>
+		<p>Referral image links are images that&#8202;&mdash;&#8202;when clicked&#8202;&mdash;&#8202;link to a chosen product on Amazon's website. You will then receive commission when people buy the product as a result of your website's referral link.</p>
+		<p>This plugin helps you to insert custom Amazon Associates referral image links into your post content.</p>
+		<p>Once you've enabled it on a post, you will see your referral image link towards the top of your post, floated to the right.</p>
 		<p><strong>Please Note: You will need an Amazon Associates account to take advantage of this plugin. You can find out more <a href='<?php echo $this->amazon_associates_link; ?>'>here</a>.</strong></p>
 		<h3>Getting Started:</h3>
 		<ol>
 			<li>Head over to the <a href='<?php echo $this->amazon_associates_link; ?>'>Amazon Associates site</a>. Register / Login and head to your <a href='<?php echo $this->amazon_dashboard_link; ?>'>dashboard</a>.</li>
 			<li>On your dashboard page you have the option of searching/browsing for a product. Find a product that you want to link to and click the yellow dropdown labelled 'Get Link'</li>
-			<li>Inside that drop down you will have a referrer link to the chosen product, which you will need for use in this plugin.</li>
+			<li>Inside that drop down you will have a referral link to the chosen product, which you will need for use in this plugin.</li>
 			<li>Once you've pasted the link somewhere safe, note the product title and type of product it is (album/film/book/etc). These will all be used in the plugin also.</li>
 			<li>Finally, you will need an image of the product that will display on your site. <strong>Please check the copyright of the image you are using before using it. I presume images taken from Amazon would be okay as it is their affiliate program but I am not 100% sure. Please check.</strong></li>
-			<li>Now you have two ways of attaching your referrer image link to your post.
+			<li>Now you have two ways of attaching your referral image link to your post.
 				<ul>
 					<li>In the editor for that particular post. (Towards the bottom of the page)</li>
 					<li>On the 'Amazon Links' page of the plugin. (All of your published posts will be listed here for the quick adding of multiple image links)</li>
@@ -110,7 +110,7 @@ class AmazonAssociatesPosts {
 			<li>To remove the settings for a particular post, check the box labelled 'Clear From Post' and click the update button.</li>
 		</ol>
 		<h3>Notes:</h3>
-		<p>At the moment when showing an amazon image link on a post, if that post normally has a featured image on the single post page, it will not be displayed. Disabling the ....</p>
+		<p>At the moment when showing an amazon referral link on a post, if that post normally has a featured image on it's single post page, it will not be displayed. To show the featured image again, set the referral link status back to 'Disabled'.</p>
 		</div>
 		<?php
 	}
